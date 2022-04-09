0eaf64ee508f50b61d14a994b84a4bc79ba37d4b
Roy Daniel <roydanielr@gmail.com>
2022-04-09 17:07:52 -0400

    rendering all 0stock properties to table

--- src/pages/Stock/Stock.js
+++ src/pages/Stock/Stock.js
@@ -14,10 +14,27 @@ export default function Stock( {stocks} ){
         console.log(curr)
     })
 
+    const getKeyValue = (obj) => {
+        const arr = []
+        for (const [key, value] of Object.entries(obj)) {
+            arr.push(
+            <tr><td>{key.charAt(0).toUpperCase() + key.slice(1)}</td><td>{value}</td></tr>
+            )
+        }
+        return arr
+    }
+
     return (
-        <div>
-            <div>Name: {thisStock.name}</div>
-            <div>Price: {thisStock.price}</div>
-        </div>
+        // <div>
+        //     <div>Name: {thisStock.name}</div>
+        //     <div>Price: {thisStock.price}</div>
+        //     <div>Change: {thisStock.change}</div>
+        //     <div>Price: {thisStock.price}</div>
+        // </div>
+        <table>
+            {
+                getKeyValue(thisStock)
+            }
+        </table>
     )
 }
