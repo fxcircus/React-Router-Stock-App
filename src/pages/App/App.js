eac12887d179aae0d80a29222612ee895d6146c1
Roy Daniel <roydanielr@gmail.com>
2022-04-09 12:34:42 -0400

    skeleton

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,6 +1,16 @@
+import Data from '../../data'
+import About from '../../pages/About/About'
+import Dashboard from '../../pages/Dashboard/Dashboard'
+import HomePage from '../../pages/HomePage/HomePage'
+import Stock from '../../pages/Stock/Stock'
 
 export default function App() {
     return (
-        <h1>hello world</h1>
+        <div>
+            <About/>
+            <Dashboard/>
+            <HomePage/>
+            <Stock/>
+        </div>
     )
 }
