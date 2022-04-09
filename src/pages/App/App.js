011dcfc6f07e5e4c3b968154f18be91c60b86679
Roy Daniel <roydanielr@gmail.com>
2022-04-09 15:09:36 -0400

    stock page

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import Data from '../../data'
 import About from '../../pages/About/About'
 import Dashboard from '../../pages/Dashboard/Dashboard'
@@ -7,13 +8,32 @@ import Stock from '../../pages/Stock/Stock'
 import Nav from '../../components/Nav'
 
 export default function App() {
+    const [ stocks, setStocks ] = useState([])
+
+    const apiKey ='f560c7d415b372b0b2362f2ee6a7eec9'
+
+    const getData = async () => {
+        try {
+            const response = await fetch(`https://financialmodelingprep.com/api/v3/stock_market/actives?apikey=${apiKey}`)
+            const data = await response.json()
+            setStocks(data)
+        } catch(err) {
+            console.error(err)
+        }
+    }    
+
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <div>
             <Nav/>
             <Routes>
                 <Route path="/" element={<HomePage/>}/>
                 <Route path="/about" element={<About/>}/>
-                <Route path="/stocks" element={<Dashboard stocks={Data}/>} />
+                <Route path="/stocks" element={<Dashboard stocks={stocks}/>} />
+                <Route path="/stocks/:symbol" element={<Stock stocks={stocks}/>}/>
             </Routes>
         </div>
     )
--- src/pages/Stock/Stock.js
+++ src/pages/Stock/Stock.js
@@ -1,3 +1,23 @@
-export default function Stock(){
-    return <h1>Stock Page</h1>
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+
+export default function Stock( {stocks} ){
+    const [thisStock, setThisStock] = useState ({})
+    const params = useParams()
+    const symbol = params.symbol
+    let curr = []
+
+
+    useEffect (() => {
+        curr = stocks.filter(stock => stock.symbol === symbol)
+        setThisStock(curr[0])
+        console.log(curr)
+    })
+
+    return (
+        <div>
+            <div>Name: {thisStock.name}</div>
+            <div>Price: {thisStock.price}</div>
+        </div>
+    )
 }
