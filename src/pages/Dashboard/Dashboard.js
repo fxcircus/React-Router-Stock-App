61fb3d931b9916812798d0de083a12b74c877978
Roy Daniel <roydanielr@gmail.com>
2022-04-09 15:57:09 -0400

    table

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,15 +1,27 @@
 import { Link } from 'react-router-dom'
 
 export default function Dashboard( { stocks }){
+    
+
     return (
-        <div className="stock-list">
+        <table>
+            <tr>
+                <th>Company Name</th>
+                <th>Price</th>
+                <th>Change</th>
+            </tr>
             {stocks.map(stock => {
                 return (
-                    <Link to={stock.symbol}>
-                        {stock.name}
-                    </Link>
+                    <tr className='row'>
+                        <Link to={stock.symbol}>
+                            <td>{stock.name}</td>
+                        </Link>
+                            <td>{stock.price}</td>
+                            <td>{stock.change} ({stock.changesPercentage})</td>
+                    </tr>
                 )
             })}
-        </div>
+            
+        </table>
     )
 }
