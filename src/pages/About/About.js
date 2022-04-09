524a0a9c437949b8735da6fb9d6179c06553f213
Roy Daniel <roydanielr@gmail.com>
2022-04-09 17:40:12 -0400

    favicon home page and about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,3 +1,10 @@
 export default function About(){
-    return <h1>About Page</h1>
+    return (
+        <div>
+            <p>A coding project using the FMP API and React Router</p>
+            <a href="https://github.com/fxcircus/React-Router-Stock-App" target="_blank">GitHub Project link</a>
+            <p>Icon: {[<span>&copy;</span>]} Mindy Miller</p>
+            <p>Code: {[<span>&copy;</span>]} Roy Daniel</p>
+        </div>
+    )
 }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,9 @@
+import { Link } from 'react-router-dom'
+
 export default function HomePage(){
-    return <h1>Home Page</h1>
+    return (
+        <Link to="/stocks">
+            <img src="/stock-app-icon-mindy.png" alt="icon"/>
+        </Link>
+    )
 }
